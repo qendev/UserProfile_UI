ce49e162c919fe58e57dcf6d502f35a8f59abd81
qendev <qenwaweru@gmail.com>
2020-03-03 13:35:41 +0300

    Initial commit

---
+++ app/src/main/java/activities/ProfileActivity.java
@@ -0,0 +1,26 @@
+package activities;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.userprofile_ui.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+}
